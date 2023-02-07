fd81da9b10e1ebf6baeef34530c485b51eaa95f9
homodeus7 <plum.bus@yahoo.com>
2023-02-07 23:00:05 +0300

    added route logic

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <main-layout></main-layout>
-  <auth-layout></auth-layout>
+  <component :is="layout + '-layout'" v-if="layout" />
 </template>
 
 <script>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
 import MainLayout from "./layout/MainLayout.vue";
 import AuthLayout from "./layout/AuthLayout.vue";
 export default {
-  setup() {},
+  setup() {
+    const route = useRoute();
+
+    return {
+      layout: computed(() => route.meta.layout),
+    };
+  },
   components: {
     MainLayout,
     AuthLayout,
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/TheHome.vue";
-
+/* eslint-disable */
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      layout: "main",
+    },
   },
   {
     path: "/help",
     name: "Help",
     component: () => import("../views/TheHelp.vue"),
+    meta: {
+      layout: "main",
+    },
   },
   {
     path: "/auth",
     name: "Auth",
     component: () => import("../views/TheAuth.vue"),
+    meta: {
+      layout: "auth",
+    },
   },
 ];
 
