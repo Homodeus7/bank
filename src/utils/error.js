e34767641df32e5430d95f2cf1fbe337e4c53e3a
homodeus7 <plum.bus@yahoo.com>
2023-02-08 20:53:35 +0300

    added authorization by password through firebase

---
+++ src/utils/error.js
@@ -0,0 +1,8 @@
+const ERROR_CODES = {
+  EMAIL_NOT_FOUND: "User with this email was not found",
+  INVALID_PASSWORD: "Unknown pass",
+};
+
+export function error(code) {
+  return ERROR_CODES[code] ? ERROR_CODES[code] : "Unknown error";
+}
